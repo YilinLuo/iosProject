4000da2815d272a8d2ee5970b771d0ea78062ac3
yilin luo <yluo21@u.rochester.edu>
2019-06-28 16:57:18 +0800

    Add files via upload

--- uiLaout2/uiLaout2/ViewController.swift
+++ uiLaout2/uiLaout2/ViewController.swift
@@ -7,8 +7,76 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
+    @IBAction func mapSearchButton(_ sender: Any) {
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+        
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //activity indicator
+        let activityIndicator = UIActivityIndicatorView(); activityIndicator.style = UIActivityIndicatorView.Style.gray
+        activityIndicator.center=self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
+       // self.view.addSubview(<#T##view: UIView##UIView#>)
+        
+        //hide search bar
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        //create the search request
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start{(response, error) in
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if response  == nil
+            {
+                print ("Error")
+            }
+            else
+            {
+                //remove annotation
+                let annotations = self.myMapView.annotations
+                self.myMapView.removeAnnotations(annotations)
+                
+                //getting data
+                let latitude = response?.boundingRegion.center.latitude
+                
+                 let longitude = response?.boundingRegion.center.longitude
+                
+                //create annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self.myMapView.addAnnotation(annotation)
+                
+                //zooming in on annotation
+                let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpan(latitudeDelta: 0.1,longitudeDelta: 0.1)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self.myMapView.setRegion(region, animated: true)
+                
+            }
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
